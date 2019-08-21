e242417c7e1526d33862f4a47698666f68a60b30
Luis <luis_aguirre_1994@outlook.com>
2019-08-20 19:01:05 -0500

    [add][url] menu & url class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LoginComponent } from './Componentes/login/login.component';
 import { PruebaComponent } from './prueba/prueba.component';
 import { RegistroComponent } from './Componentes/registro/registro.component';
 import { MaestroComponent } from './Componentes/maestro/maestro.component';
+import { MateriaComponent } from './Componentes/materia/materia.component';
 
 const routes: Routes = [
   { path:'login', component: LoginComponent },
   // { path:'login', component: RegistroComponent },
   { path:'prueba', component: PruebaComponent },
   { path:'RegistroMaestro',component:MaestroComponent},
+  { path:'RegistroMateria', component:MateriaComponent},
+
 ];
 
 @NgModule({
