be879257c2e49b550b4f969ab2d04dae00d9ca8e
mavericc <escobarveric@gmail.com>
2019-08-25 05:24:11 -0500

    front calificaciones

--- src/app/Componentes/calificaciones/calificaciones.component.ts
+++ src/app/Componentes/calificaciones/calificaciones.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables/index';
 import * as $ from 'jquery';
 import { CalificacionesService } from 'src/app/Servicios/calificaciones.service';
-// import 'datatables.net';
+import { Subject } from 'rxjs';
+
+import 'datatables.net';
 
 @Component({
   selector: 'app-calificaciones',
@@ -16,114 +18,71 @@ export class CalificacionesComponent implements OnInit {
   dt: any;
   dtOptions: any = {};
   indexes: any = [];
+  // dtTrigger: Subject<any> = new Subject();
+  busco: Boolean = false;
 
   constructor(public service: CalificacionesService) { }
 
   ngOnInit() {
+    // $('#datat').DataTable().destroy();
+  }
 
-    var datos = [
-      {
-        "Matricula":16090011,
-        "Nombre":"Erick Miguel",
-        "Apellido_paterno":"Escobar",
-        "Apellido_materno":"Vázquez",
-        "calificaciones": [{
-          "id_materia": 666,
-          "unidad": 1,
-          "calificacion": 666
-        }, {
-          "id_materia": 666,
-          "unidad": 2,
-          "calificacion": 69
-        }, {
-          "id_materia": 666,
-          "unidad": 3,
-          "calificacion": 420
-        },        
-        // {
-        //   "id_materia": 666,
-        //   "unidad": 4,
-        //   "calificacion": 777
-        // }
-      ]
-      },
-      {
-        "Matricula":16090021,
-        "Nombre":"Luis Ángel",
-        "Apellido_paterno":"García",
-        "Apellido_materno":"",
-        "calificaciones": [{
-          "id_materia": 666,
-          "unidad": 1,
-          "calificacion": 61
-        }, {
-          "id_materia": 666,
-          "unidad": 2,
-          "calificacion": 15
-        }, {
-          "id_materia": 666,
-          "unidad": 3,
-          "calificacion": 20
-        },        
-        // {
-        //   "id_materia": 666,
-        //   "unidad": 4,
-        //   "calificacion": 777
-        // }
-      ]
-      },
-      // {
-      //   "Matricula":16090031,
-      //   "Nombre":"Ángel Octavio",
-      //   "Apellido_paterno":"García",
-      //   "Apellido_materno":"García",
-      //   "calificaciones": [{
-      //     "id_materia": 666,
-      //     "unidad": 1,
-      //     "calificacion": 6
-      //   }, {
-      //     "id_materia": 666,
-      //     "unidad": 2,
-      //     "calificacion": 9
-      //   }, {
-      //     "id_materia": 66,
-      //     "unidad": 3,
-      //     "calificacion": 40
-      //   },        
-      //   // {
-      //   //   "id_materia": 666,
-      //   //   "unidad": 4,
-      //   //   "calificacion": 777
-      //   // }
-      // ]
-      // },
-      // {
-      //   "Matricula":16090041,
-      //   "Nombre":"Jaime Emmanuel",
-      //   "Apellido_paterno":"Burciaga",
-      //   "Apellido_materno":"Medina",
-      //   "calificaciones": [{
-      //     "id_materia": 666,
-      //     "unidad": 1,
-      //     "calificacion": 66
-      //   }, {
-      //     "id_materia": 666,
-      //     "unidad": 2,
-      //     "calificacion": 6
-      //   }, {
-      //     "id_materia": 666,
-      //     "unidad": 3,
-      //     "calificacion": 42
-      //   }, 
-      //   // {
-      //   //   "id_materia": 666,
-      //   //   "unidad": 4,
-      //   //   "calificacion": 777
-      //   // }
-      // ]
-      // }
-    ];
+  displayToConsole(datatableElement: DataTableDirective): void {
+    datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      var calificaciones = [];
+      
+      this.indexes.forEach(element => {
+        $(dtInstance.column(element).nodes()).find('input').each(function () {
+            var renglon = dtInstance.row($(this).parents('tr')).index();
+            var rowData = dtInstance.row($(this).parents('tr')).data();
+            //@ts-ignore
 
+            var calificacion = dtInstance.cell(renglon, element).nodes().to$().find('input').val();
+            var alumno = {
+              Matricula: rowData[0],
+              id_materia: $("#materia :selected").val(),
+              unidad: element - 1,
+              calificacion: parseFloat(calificacion)
+            };
+
+            calificaciones.push(alumno);
+        });
+      });
+
+      this.service.calificarAlumnos(calificaciones).subscribe((res) => {
+        debugger;
+      });
+    });
+  }
+
+  calificar() {
+
+  }
+
+  buscar() {
+    var grado = $("#grado :selected").val();
+    var seccion = $("#seccion :selected").val();
+    var ciclo = $("#ciclo_escolar :selected").val();
+    var materia = $("#materia :selected").val();
+
+    this.service.buscarSalon(grado, seccion, ciclo, materia).subscribe(res => {
+      
+      var dataToSend = [];
+
+      res.salon.Alumnos.forEach(alumno => {
+        dataToSend.push({
+          Nombre: alumno.Nombre,
+          Apellido_paterno: alumno.Apellido_paterno,
+          Apellido_materno: alumno.Apellido_materno,
+          calificaciones: alumno.calificaciones
+        })
+      });
+      this.displayTable(dataToSend)
+    });
+  }
+
+  displayTable(datos) {
+    // $('#datat').DataTable().destroy();
     datos.forEach((el) => {
       el.calificaciones.forEach((cal) => {
         el['U' + cal.unidad] = cal.calificacion; //de la propiedad calificaciones (Arreglo) se saca la unidad y agregan como propiedad a cada alumno
@@ -158,87 +117,84 @@ export class CalificacionesComponent implements OnInit {
       e inserta en otro, el cual dinámicamente contendrá las posiciones de las columnas pertenecientes a las unidades:
       a cada celda en esa columna se le aplicará el input editable
     */
-    var colIndexes = [];    
-    Object.keys(columns).slice(2).forEach((el) => {  colIndexes.push(parseInt(el))});
-    this.indexes = colIndexes;
-    //3>4-X
-    this.dtOptions = {
-      dom: 'Bfrtip',
-      // Configure the buttons
-      buttons: [
-        'copy',
-        'print',
-        'excel',
-        'pdf',
-      ],
-      pagingType: 'full_numbers',
-      // language: "/otros_scripts/Spanish.json",
-      language: {
-        "sProcessing":     "Procesando...",
-        "sLengthMenu":     "Mostrar _MENU_ registros",
-        "sZeroRecords":    "No se encontraron resultados",
-        "sEmptyTable":     "Ningún dato disponible en esta tabla",
-        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-        "sInfoPostFix":    "",
-        "sSearch":         "Buscar:",
-        "sUrl":            "",
-        "sInfoThousands":  ",",
-        "sLoadingRecords": "Cargando...",
-        "oPaginate": {
-          "sFirst":    "Primero",
-          "sLast":     "Último",
-          "sNext":     "Siguiente",
-          "sPrevious": "Anterior"
+
+   var colIndexes = [];    
+   Object.keys(columns).slice(1).forEach((el) => {  colIndexes.push(parseInt(el))});
+   this.indexes = colIndexes;
+   //3>4-X
+
+   if (this.busco) {
+    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+    dtInstance.destroy();
+   }); 
+   }
+   
+
+   this.dtOptions = {
+    dom: 'Bfrtip',
+    // Configure the buttons
+    buttons: [
+      'copy',
+      'print',
+      'excel',
+      'pdf',
+    ],
+    pagingType: 'full_numbers',
+    // language: "/otros_scripts/Spanish.json",
+    language: {
+      "sProcessing":     "Procesando...",
+      "sLengthMenu":     "Mostrar _MENU_ registros",
+      "sZeroRecords":    "No se encontraron resultados",
+      "sEmptyTable":     "Ningún dato disponible en esta tabla",
+      "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+      "sInfoPostFix":    "",
+      "sSearch":         "Buscar:",
+      "sUrl":            "",
+      "sInfoThousands":  ",",
+      "sLoadingRecords": "Cargando...",
+      "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+      }
+    },
+    data: datat,
+    columns: columns,
+      columnDefs: [
+        {
+          render: function (data, type, row, meta) {
+            //columndefs permite editar la data para display.
+            //se obtiene la propiedad dicha por el objeto meta.col = la calificación de la unidad
+            var cal = row[meta.col];
+            return '<input class="calcular form-control text-box single-line" style="text-align:right" value="' + cal +'">';
+          },
+          targets: colIndexes //[1, 2, 3, 4, 5........]]
         }
-      },
-      data: datat,
-      columns: columns,
-        columnDefs: [
-          {
-            render: function (data, type, row, meta) {
-              //columndefs permite editar la data para display.
-              //se obtiene la propiedad dicha por el objeto meta.col = la calificación de la unidad
-              var cal = row[meta.col];
-              return '<input class="calcular form-control text-box single-line" style="text-align:right" value="' + cal +'">';
-            },
-            targets: colIndexes //[1, 2, 3, 4, 5........]]
-          }
-        ]
-      };
-  }
-
-  displayToConsole(datatableElement: DataTableDirective): void {
-    datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
-      var calificaciones = [];
-      
-      this.indexes.forEach(element => {
-        $(dtInstance.column(element).nodes()).find('input').each(function () {
-            var renglon = dtInstance.row($(this).parents('tr')).index();
-            var rowData = dtInstance.row($(this).parents('tr')).data();
-            //@ts-ignore
-
-            var calificacion = dtInstance.cell(renglon, element).nodes().to$().find('input').val();
-            var alumno = {
-              Matricula: rowData[0],
-              id_materia: $("#materia :selected").val(),
-              unidad: element - 1,
-              calificacion: parseFloat(calificacion)
-            };
-
-            calificaciones.push(alumno);
-        });
-      });
-
-      this.service.calificarAlumnos(calificaciones).subscribe((res) => {
-        debugger;
-      });
-    });
-  }
-
-  calificar() {
+      ]
+    };
+    debugger;
+    this.busco = true;
 
   }
+
+  // ngAfterViewInit(): void {
+  //   this.dtTrigger.next();
+  // }
+
+  // ngOnDestroy(): void {
+  //   // Do not forget to unsubscribe the event
+  //   this.dtTrigger.unsubscribe();
+  // }
+
+  // rerender(): void {
+  //   this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+  //     // Call the dtTrigger to rerender again
+  //     dtInstance.optio
+  //     this.dtTrigger.next();
+  //   });
+  // }
 
 }
